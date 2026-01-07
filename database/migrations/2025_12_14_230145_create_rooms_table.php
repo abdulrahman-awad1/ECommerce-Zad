ab89dba3600f7036731e7d19e6bf882b47a681d0
abdulrahman-awad1 <aulrahman034@gmail.com>
2026-01-07 19:04:10 +0200

    room

--- database/migrations/2025_12_14_230145_create_rooms_table.php
+++ database/migrations/2025_12_14_230145_create_rooms_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             
@@ -25,6 +26,7 @@ return new class extends Migration
 
             $table->timestamps();
         });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
